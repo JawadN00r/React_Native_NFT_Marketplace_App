f1d917a2c61bfe47b9883461cd8bc428c1fe0eac
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2022-05-14 23:36:09 +0600

    nft card done

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -31,13 +31,28 @@ const NFTCard = ({ data }) => {
         <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
       <SubInfo />
-      <View
-        style={{
-          width: "100%",
-          padding: SIZES.font,
-        }}
-      >
-        <NFTTitle />
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
       </View>
     </View>
   );
