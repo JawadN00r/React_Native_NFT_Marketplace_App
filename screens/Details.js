5905ce50ec9ce22965b814471bcef651435f716e
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2022-05-14 15:56:29 +0600

    assets loaded

---
+++ screens/Details.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native-web";
+
+const Details = () => {
+  return <Text>Details</Text>;
+};
+
+export default Details;
---
+++ screens/Home.js
@@ -0,0 +1,6 @@
+import { Text } from "react-native";
+const Home = () => {
+  return <Text>Home</Text>;
+};
+
+export default Home;
